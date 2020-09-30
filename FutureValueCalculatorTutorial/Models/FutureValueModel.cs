88820dfa5a765334a3171a2a552ff92794f3ebf6
Frank Eslami <eslamifrank@gmail.com>
2020-09-30 07:51:09 -0700

    Added additional validation.

--- FutureValueCalculatorTutorial/Models/FutureValueModel.cs
+++ FutureValueCalculatorTutorial/Models/FutureValueModel.cs
@@ -8,12 +8,15 @@ using System.Threading.Tasks;
 namespace FutureValueCalculatorTutorial.Models {
     public class FutureValueModel {
         [Required(ErrorMessage = "Please enter a monthly investment.")]
+        [Range(1, 500, ErrorMessage ="Monthly investment must be between 1 and 500.")]
         public decimal? MonthlyInvestment { get; set; }
 
         [Required(ErrorMessage = "Please enter an interest rate.")]
+        [Range(0.1, 10.0, ErrorMessage ="Yearly interest rate must be between 0.1 and 10.0.")]
         public decimal? YearlyInterestRate { get; set; }
-        
+
         [Required(ErrorMessage = "Please enter the number of years.")]
+        [Range(1, 50, ErrorMessage ="Number of years must be between 1 and 50.")]
         public int? Years { get; set; }
 
         public decimal? Calculate() {
