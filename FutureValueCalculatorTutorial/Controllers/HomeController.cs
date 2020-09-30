c92f5a59b23165cfc51d958dbf59423ce19f880c
Frank Eslami <eslamifrank@gmail.com>
2020-09-30 07:40:02 -0700

    Initial files.

---
+++ FutureValueCalculatorTutorial/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FutureValueCalculatorTutorial.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FutureValueCalculatorTutorial.Controllers {
+    public class HomeController : Controller {
+        [HttpGet]
+        public IActionResult Index() {
+            ViewBag.FutureValue = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel futureValue) {
+            if (ModelState.IsValid) {
+                ViewBag.FutureValue = futureValue.Calculate().ToString("c2");
+            }
+            else {
+                ViewBag.FutureValue = "";
+            }
+
+            return View();
+        }
+    }
+}
---
+++ FutureValueCalculatorTutorial/Models/FutureValueModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+
+namespace FutureValueCalculatorTutorial.Models {
+    public class FutureValueModel {
+        [Required(ErrorMessage = "Please enter a monthly investment.")]
+        public decimal MonthlyInvestment { get; set; }
+
+        [Required(ErrorMessage = "Please enter an interest rate.")]
+        public decimal YearlyInterestRate { get; set; }
+        
+        [Required(ErrorMessage = "Please enter the number of years.")]
+        public int Years { get; set; }
+
+        public decimal Calculate() {
+            int months = Years * 12;
+            decimal monthlyInterestRate = YearlyInterestRate / 12 / 100;
+            decimal futureValue = 0;
+
+            for (int i = 0; i < months; i++) {
+                futureValue = (futureValue + MonthlyInvestment) * (1 + monthlyInterestRate);
+            }
+
+            return futureValue;
+        }
+    }
+}
